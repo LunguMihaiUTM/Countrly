152f6f041fa13c06f7fa177e0b577020a4291def
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-10-29 22:31:16 +0200

    add the enum for complexity

--- src/main/java/com/agin/countrly/enums/ComplexityEnum.java
+++ src/main/java/com/agin/countrly/enums/ComplexityEnum.java
@@ -1,7 +1,12 @@
 package com.agin.countrly.enums;
 
 public enum ComplexityEnum {
-    Easy,
-    Medium,
-    Hard
+    EASY,
+    MEDIUM,
+    HARD;
+
+    @Override
+    public String toString() {
+        return this.name(); // Sau returnezi direct name()
+    }
 }
