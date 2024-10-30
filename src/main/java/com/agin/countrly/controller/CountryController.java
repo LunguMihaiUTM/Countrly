10853e96c9b630725521893d18c8bba38b5edf98
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-10-30 14:38:05 +0200

    add the method to get countries by filter

--- src/main/java/com/agin/countrly/controller/CountryController.java
+++ src/main/java/com/agin/countrly/controller/CountryController.java
@@ -1,8 +1,6 @@
 package com.agin.countrly.controller;
-import com.agin.countrly.dto.request.ComplexityFilterDTO;
 import com.agin.countrly.dto.response.CountryDTO;
 import com.agin.countrly.enums.ComplexityEnum;
-import com.agin.countrly.service.ComplexityService;
 import com.agin.countrly.service.CountryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +13,15 @@ import java.util.List;
 @RequestMapping("/country")
 public class CountryController {
     private final CountryService countryService;
-    private final ComplexityService complexityService;
 
     @GetMapping
     public ResponseEntity<List<CountryDTO>> getAllCountries(){
         return ResponseEntity.ok(countryService.getAllCountries());
     }
 
-    @PostMapping("/complexity")
-    public ResponseEntity<List<CountryDTO>> getCountriesByComplexity(@RequestBody(required = false) ComplexityEnum filter){
-        return ResponseEntity.ok(complexityService.getCountriesByComplexity(filter));
+    @PostMapping("/complexity/{filter}")
+    public ResponseEntity<List<CountryDTO>> getCountriesByComplexity (@PathVariable ComplexityEnum filter){
+        return ResponseEntity.ok(countryService.getCountriesByComplexity(filter));
     }
 
 
