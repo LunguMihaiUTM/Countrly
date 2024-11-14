9a9f1bff9e383e285327ecf2895ac614207e8a59
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-14 17:33:21 +0200

    update the webconfig

--- src/main/java/com/agin/countrly/config/WebConfig.java
+++ src/main/java/com/agin/countrly/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://exciting-wonder-production.up.railway.app");
+        registry.addMapping("/**")
+                .allowedOrigins("https://exciting-wonder-production.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
