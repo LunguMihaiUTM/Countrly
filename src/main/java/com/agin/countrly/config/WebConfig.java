f1d8b02c3e6eff707b1802d6b05b9f215d0da50e
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-14 18:14:26 +0200

    small fix

--- src/main/java/com/agin/countrly/config/WebConfig.java
+++ src/main/java/com/agin/countrly/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://exciting-wonder-production.up.railway.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
