2e5e8909f607b174ac1aee5ee4814f2ed428960c
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-17 20:17:00 +0200

    Updated the user relation

--- src/main/resources/db/changelog/changes/001-create-users-table.sql
+++ src/main/resources/db/changelog/changes/001-create-users-table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
     id          BIGINT       NOT NULL PRIMARY KEY,
-    username    VARCHAR(255) NOT NULL,
-    email       VARCHAR(255) NOT NULL,
+    username    VARCHAR(255) NOT NULL UNIQUE,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    role        VARCHAR(255) NOT NULL,
     password    VARCHAR(255) NOT NULL
+
 );
 
 ALTER TABLE users OWNER TO postgres;
--- src/main/resources/db/changelog/changes/006-populate-users-tables.sql
+++ src/main/resources/db/changelog/changes/006-populate-users-tables.sql
@@ -1,11 +1,11 @@
-INSERT INTO users (id, username, email, password)
-VALUES (nextval('user_id_seq'), 'Vito_Scaletta', 'vito@example.com', 'password123'),
-       (nextval('user_id_seq'), 'Ryan_Gosling', 'ryan@example.com', 'securepass'),
-       (nextval('user_id_seq'), 'Antonio_Margaritti', 'harold@example.com', 'mypassword'),
-       (nextval('user_id_seq'), 'Thomas_Shelby', 'success.kid@example.com', 'password456'),
-       (nextval('user_id_seq'), 'Tony_Montana', 'distracted.boyfriend@example.com', 'charliepass'),
-       (nextval('user_id_seq'), 'Bruce_Lee', 'grumpy.cat@example.com', 'grumpy123'),
-       (nextval('user_id_seq'), 'Waltuh_White', 'travolta@example.com', 'travolta456'),
-       (nextval('user_id_seq'), 'Fernando_Alonso', 'ancient.aliens@example.com', 'aliensrule'),
-       (nextval('user_id_seq'), 'Uvuvwevwevwe_Onyetenyevwe_Ugwemubwem_Ossas', 'kermit@example.com', 'siptea789'),
-       (nextval('user_id_seq'), 'Elon_Musk', 'drake@example.com', 'drakepass');
+INSERT INTO users (id, username, email, role, password)
+VALUES (nextval('user_id_seq'), 'Vito_Scaletta', 'vito@example.com','USER', 'password123'),
+       (nextval('user_id_seq'), 'Ryan_Gosling', 'ryan@example.com','USER', 'securepass'),
+       (nextval('user_id_seq'), 'Antonio_Margaritti', 'harold@example.com','USER', 'mypassword'),
+       (nextval('user_id_seq'), 'Thomas_Shelby', 'success.kid@example.com','USER', 'password456'),
+       (nextval('user_id_seq'), 'Tony_Montana', 'distracted.boyfriend@example.com','USER', 'charliepass'),
+       (nextval('user_id_seq'), 'Bruce_Lee', 'grumpy.cat@example.com','USER', 'grumpy123'),
+       (nextval('user_id_seq'), 'Waltuh_White', 'travolta@example.com','USER', 'travolta456'),
+       (nextval('user_id_seq'), 'Fernando_Alonso', 'ancient.aliens@example.com','USER', 'aliensrule'),
+       (nextval('user_id_seq'), 'Uvuvwevwevwe_Onyetenyevwe_Ugwemubwem_Ossas', 'kermit@example.com', 'USER', 'siptea789'),
+       (nextval('user_id_seq'), 'Elon_Musk', 'drake@example.com','USER', 'drakepass');
