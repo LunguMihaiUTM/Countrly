8bea433b1390bd56904f96debba8302cf56d6645
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-17 20:14:16 +0200

    Added new DTO's fro authentication

---
+++ src/main/java/com/agin/countrly/dto/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.agin.countrly.dto.request;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
