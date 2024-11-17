4c3e55daf7e9a4acf6be537bf6404e5fe4fb8af5
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-17 20:16:21 +0200

    Added new services for the authentication

---
+++ src/main/java/com/agin/countrly/service/AuthService.java
@@ -0,0 +1,4 @@
+package com.agin.countrly.service;
+
+public interface AuthService {
+}
---
+++ src/main/java/com/agin/countrly/service/impl/AuthServiceImpl.java
@@ -0,0 +1,55 @@
+package com.agin.countrly.service.impl;
+
+import com.agin.countrly.dto.request.LoginRequest;
+import com.agin.countrly.dto.request.RegisterRequest;
+import com.agin.countrly.dto.response.AuthenticationResponse;
+import com.agin.countrly.entity.User;
+import com.agin.countrly.enums.Role;
+import com.agin.countrly.repository.UserRepository;
+import com.agin.countrly.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtServiceImpl jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .username(user.getUsername())
+                .build();
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .username(user.getUsername())
+                .build();
+    }
+}
