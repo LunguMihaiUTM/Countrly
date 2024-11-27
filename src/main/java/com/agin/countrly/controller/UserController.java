779e02b6fc202f721dec05d50f06247edde5609b
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-27 15:37:17 +0200

    fixed the user controller

--- src/main/java/com/agin/countrly/controller/UserController.java
+++ src/main/java/com/agin/countrly/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
