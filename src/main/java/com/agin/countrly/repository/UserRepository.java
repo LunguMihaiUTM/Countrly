a8bc8e11552f330ecf7e7a6eff536da72a52772c
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-28 16:22:53 +0200

    Added a custom query for geting users from the database in a secure way

--- src/main/java/com/agin/countrly/repository/UserRepository.java
+++ src/main/java/com/agin/countrly/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.agin.countrly.repository;
 
+import com.agin.countrly.dto.response.UserDTO;
 import com.agin.countrly.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    // password will create an error
+    @Query("SELECT new com.agin.countrly.entity.User(u.id, u.username) FROM users u ")
+    List<User> findAllUsersWithoutPassword();
+
+    // password will create an error
+    @Query("SELECT new com.agin.countrly.entity.User(u.id, u.username) FROM users u WHERE u.id = :id")
+    Optional<User> findUserWithoutPasswordById(@Param("id") Long id);
 }
